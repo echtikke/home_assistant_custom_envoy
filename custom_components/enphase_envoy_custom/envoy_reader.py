79810777aec7361f3cfdb15594f343436058f9d5
echtikke <92547387+echtikke@users.noreply.github.com>
2023-07-12 15:30:59 +0200

    Update envoy_reader.py

--- custom_components/enphase_envoy_custom/envoy_reader.py
+++ custom_components/enphase_envoy_custom/envoy_reader.py
@@ -654,7 +654,7 @@ class EnvoyReader:  # pylint: disable=too-many-instance-attributes
         elif self.endpoint_type == ENVOY_MODEL_C or (
             self.endpoint_type == ENVOY_MODEL_S and not self.isMeteringEnabled
         ):
-            raw_json = self.endpoint_production_v1_results.json()
+            raw_json = self.endpoint_production_json_results.json()
             lifetime_production = raw_json["production"][0]["whLifetime"]
         elif self.endpoint_type == ENVOY_MODEL_LEGACY:
             text = self.endpoint_production_results.text
